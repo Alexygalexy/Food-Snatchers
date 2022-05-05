eca5eb2b14c077f624ba0c7e2802c490204d5f0e
Alexygalexy <79289040+Alexygalexy@users.noreply.github.com>
2022-05-05 16:16:31 +0200

    Player Nav Mesh and follow target

---
+++ Actual Food Snatchers/Assets/Scripts/Alex Scripts/Player_Nav_Mesh.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player_Nav_Mesh : MonoBehaviour
+{
+    [SerializeField] private Transform movePositionTransform;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}
