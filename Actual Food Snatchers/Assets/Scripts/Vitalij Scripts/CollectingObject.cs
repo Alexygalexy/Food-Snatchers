299c24734c7e6865523cc868ab4e7ffa4adbfc77
Vitalij Legeza <v.legeza@outlook.com>
2022-05-18 16:03:52 +0200

    WIP stealing objects
    Working on stealing objects after colliding into other player

--- Actual Food Snatchers/Assets/Scripts/Vitalij Scripts/CollectingObject.cs
+++ Actual Food Snatchers/Assets/Scripts/Vitalij Scripts/CollectingObject.cs
@@ -6,13 +6,30 @@ public class CollectingObject : MonoBehaviour
 {
 
     public GameObject player;
+    public int numberOfFood;
 
     void OnTriggerEnter(Collider other)
     {
         {
             other.transform.parent = player.transform;
             other.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
+            numberOfFood++;
+            Debug.Log(numberOfFood);
         }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(numberOfFood >= 1)
+        {
+            numberOfFood--;
+            Debug.Log(numberOfFood);
+        }
+        if(numberOfFood == 0)
+        {
+            Debug.Log("Nothing to steal");
+            
+        }
+
     }
 
 }
