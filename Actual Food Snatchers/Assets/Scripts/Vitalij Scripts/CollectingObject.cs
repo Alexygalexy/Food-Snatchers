eae317d39d96297a4fd6c1bfd9968d23e2d4d789
Vitalij Legeza <v.legeza@outlook.com>
2022-05-18 15:12:21 +0200

    WIP picking up object
    Cleaned from unnecessary stuff

--- Actual Food Snatchers/Assets/Scripts/Vitalij Scripts/CollectingObject.cs
+++ Actual Food Snatchers/Assets/Scripts/Vitalij Scripts/CollectingObject.cs
@@ -6,32 +6,13 @@ public class CollectingObject : MonoBehaviour
 {
 
     public GameObject player;
-    // public GameObject cube;
-
-
-    // void update()
-    // {
-    //     // player = GameObject.Find("Player");
-    //     // Debug.Log("Found Player!");
-    // }
-    
 
     void OnTriggerEnter(Collider other)
     {
-        
         {
-            Instantiate(other.gameObject, new Vector3(-7, 2, 1), Quaternion.identity);
-            Debug.Log("Collected! One point!");
             other.transform.parent = player.transform;
             other.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
-            other.gameObject.SetActive(false);
         }
     }
 
-    void OnCollisionEnter(Collision player)
-    {
-        // cube = GameObject.Find("Cube");
-        // Physics.IgnoreCollision(cube.GetComponent<Collider>(), GetComponent <Collider>());
-        
-    }
 }
