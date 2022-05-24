024f868b58e616bf9f18a6d5b901ad01b05af8e2
Alexygalexy <79289040+Alexygalexy@users.noreply.github.com>
2022-05-24 12:22:01 +0200

    State Machine start

---
+++ Actual Food Snatchers/Assets/Scripts/Alex Scripts/Bots/Alex_Bot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alex_Bot : AI_System
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    #region Main Methods
+
+    //
+    #endregion
+}
