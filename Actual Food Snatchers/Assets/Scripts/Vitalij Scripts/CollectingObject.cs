110b3cd41f2794ade36c01f32651b3f3c27fcf2b
Vitalij Legeza <v.legeza@outlook.com>
2022-05-25 15:40:21 +0200

    Finished working on score, commented out many lines
    Various pick-up objects give various amount of points
    Score is hanging above player's head
    Commented out lines of code that probably won't be needed in final product.

--- Actual Food Snatchers/Assets/Scripts/Vitalij Scripts/CollectingObject.cs
+++ Actual Food Snatchers/Assets/Scripts/Vitalij Scripts/CollectingObject.cs
@@ -19,23 +19,49 @@ public class CollectingObject : MonoBehaviour
     // Changes this GameObject's tag, so that this GameObject stop looking for other GameObjects
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Food")
+            if (other.gameObject.tag == "Apple")
         {
+            Score++;
+            player1_scoreText.text = Score.ToString();
+        }
+        else if (other.gameObject.tag == "Banana")
+        {
+            Score+=2;
+            player1_scoreText.text = Score.ToString();
+        }
+        else if (other.gameObject.tag == "Blueberry")
+        {
+            Score+=3;
+            player1_scoreText.text = Score.ToString();
+        }
+        else if (other.gameObject.tag == "Grapes")
+        {
+            Score+=4;
+            player1_scoreText.text = Score.ToString();
+        }
+        else if (other.gameObject.tag == "Orange")
+        {
+            Score+=5;
+            player1_scoreText.text = Score.ToString();
+        }
+            Destroy(other.gameObject);
+            // player.gameObject.tag = "Passive";
+        }
+        // if (other.gameObject.tag == "Food")
+        // {
             // other.transform.parent = player.transform;
             // other.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
-            AddPoints();
-            Destroy(other.gameObject);
-            player.gameObject.tag = "Passive";
-        }
-    }
+            // AddPoints();
+        
+    // }
 
     // Raises score by one
     // Writes current player 1's score on canvas 
-    void AddPoints()
-    {
-        Score++;
-        player1_scoreText.text = Score.ToString();
-    }
+    // void AddPoints()
+    // {
+        
+
+    // }
 
     //When this GameObject collides with other GameObject
     void OnCollisionEnter(Collision other)
@@ -53,42 +79,42 @@ public class CollectingObject : MonoBehaviour
             player1_scoreText.text = Score.ToString();
             this.transform.position = new Vector3(transform.position.x - 3, transform.position.y - 3, transform.position.z);
         }
-        if (Score < 1)
-        {
-            // Disables stealing GameObject's NavMesh, so that it won't chases other GameObjects
-            GameObject.FindGameObjectWithTag("Stealing").GetComponent<Player_Nav_Mesh>().enabled = false;
-        }
+        // if (Score < 1)
+        // {
+        //     // Disables stealing GameObject's NavMesh, so that it won't chases other GameObjects
+        //     GameObject.FindGameObjectWithTag("Stealing").GetComponent<Player_Nav_Mesh>().enabled = false;
+        // }
     }
 
     //  Disables and enables this GameObject's NavMesh, which makes it stop looking for other GameObjects
-    void makePassive()
-    {
-        if (!madePassive)
-        {
-            if (gameObject.tag == "Passive")
-            {
+    // void makePassive()
+    // {
+    //     if (!madePassive)
+    //     {
+    //         if (gameObject.tag == "Passive")
+    //         {
 
-                Debug.Log(this.gameObject.name + " is Passive");
-                this.GetComponent<Player_Nav_Mesh>().enabled = false;
-                this.GetComponent<Player_Nav_Mesh>().enabled = true;
-                madePassive = true;
-                startChasing();
-            }
-        }
-    }
+    //             Debug.Log(this.gameObject.name + " is Passive");
+    //             this.GetComponent<Player_Nav_Mesh>().enabled = false;
+    //             this.GetComponent<Player_Nav_Mesh>().enabled = true;
+    //             madePassive = true;
+    //             // startChasing();
+    //         }
+    //     }
+    // }
     // Enables stealing GameObject's NavMesh, so that it starts chasing other GameObjects
-    void startChasing()
-    {
-        if (gameObject.tag == "Passive")
-        {
+    // void startChasing()
+    // {
+    //     if (gameObject.tag == "Passive")
+    //     {
 
-            Debug.Log("Start Stealing");
-            GameObject.FindGameObjectWithTag("Stealing").GetComponent<Player_Nav_Mesh>().enabled = true;
-        }
-    }
-    void Update()
-    {
-        makePassive();
-    }
+    //         Debug.Log("Start Stealing");
+    //         GameObject.FindGameObjectWithTag("Stealing").GetComponent<Player_Nav_Mesh>().enabled = true;
+    //     }
+    // }
+    // void Update()
+    // {
+    //     makePassive();
+    // }
 
 }
