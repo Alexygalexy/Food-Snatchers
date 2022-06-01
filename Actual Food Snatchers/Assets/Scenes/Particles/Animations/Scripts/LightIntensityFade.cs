087e65a4ecece83a291b165c60c904fb7cadc6a9
TomKlein88 <67271512+TomKlein88@users.noreply.github.com>
2022-06-01 15:55:03 +0200

    Light scripts fix

--- Actual Food Snatchers/Assets/Scenes/Particles/Animations/Scripts/LightIntensityFade.cs
+++ Actual Food Snatchers/Assets/Scenes/Particles/Animations/Scripts/LightIntensityFade.cs
@@ -1,65 +1,71 @@
 using UnityEngine;
 using System.Collections;
 
-// Cartoon FX  - (c) 2015 Jean Moreno
-
-// Decreases a light's intensity over time.
-
 [RequireComponent(typeof(Light))]
-public class CFX_LightIntensityFade : MonoBehaviour
+public class LightIntensityFade : MonoBehaviour
 {
-	// Duration of the effect.
-	public float duration = 1.0f;
-	
-	// Delay of the effect.
-	public float delay = 0.0f;
-	
-	/// Final intensity of the light.
-	public float finalIntensity = 0.0f;
-	
-	// Base intensity, automatically taken from light parameters.
-	private float baseIntensity;
-	
-	// If <c>true</c>, light will destructs itself on completion of the effect
-	public bool autodestruct;
-	
-	private float p_lifetime = 0.0f;
-	private float p_delay;
-	
-	void Start()
-	{
-		baseIntensity = GetComponent<Light>().intensity;
-	}
-	
-	void OnEnable()
-	{
-		p_lifetime = 0.0f;
-		p_delay = delay;
-		if(delay > 0) GetComponent<Light>().enabled = false;
-	}
-	
-	void Update ()
-	{
-		if(p_delay > 0)
-		{
-			p_delay -= Time.deltaTime;
-			if(p_delay <= 0)
-			{
-				GetComponent<Light>().enabled = true;
-			}
-			return;
-		}
-		
-		if(p_lifetime/duration < 1.0f)
-		{
-			GetComponent<Light>().intensity = Mathf.Lerp(baseIntensity, finalIntensity, p_lifetime/duration);
-			p_lifetime += Time.deltaTime;
-		}
-		else
-		{
-			if(autodestruct)
-				GameObject.Destroy(this.gameObject);
-		}
-		
-	}
+    // Duration of the effect.
+    [SerializeField]
+    private float duration = 1.0f;
+
+    // Delay of the effect.
+    [SerializeField]
+    private float delay = 0.0f;
+
+    /// Final intensity of the light.
+    [SerializeField]
+    private float finalIntensity = 0.0f;
+
+    // Base intensity, automatically taken from light parameters.
+    [SerializeField]
+    private float baseIntensity;
+
+    // If <c>true</c>, light will destructs itself on completion of the effect
+    [SerializeField]
+    private bool autodestruct;
+
+    [SerializeField]
+    private float lifetime = 0.0f;
+
+    [SerializeField]
+    private float duration_delay;
+
+
+    void Start()
+    {
+        Light light = GetComponent<Light>();
+        baseIntensity = light.intensity;
+    }
+
+    void OnEnable()
+    {
+        lifetime = 0.0f;
+        duration_delay = delay;
+        if (delay > 0) light.enabled = false;
+    }
+
+    void Update()
+    {
+        if (duration_delay > 0)
+        {
+            duration_delay -= Time.deltaTime;
+            if (duration_delay <= 0)
+            {
+                light.enabled = true;
+            }
+            return;
+        }
+
+        if (lifetime / duration < 1.0f)
+        {
+            light.intensity = Mathf.Lerp(baseIntensity, finalIntensity, lifetime / duration);
+            lifetime += Time.deltaTime;
+        }
+        else
+        {
+            if (autodestruct)
+                GameObject.Destroy(this.gameObject);
+        }
+
+    }
 }
