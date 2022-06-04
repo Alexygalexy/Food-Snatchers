f0c1de4ce81237e04220541ca95d21c2b7b17a50
Sjoeke <s.m.de.vries@st.hanze.nl>
2022-06-04 16:44:29 +0200

    Update Sjoeke_AI.cs

--- Actual Food Snatchers/Assets/Scripts/Sjoeke Scripts/Sjoeke_AI.cs
+++ Actual Food Snatchers/Assets/Scripts/Sjoeke Scripts/Sjoeke_AI.cs
@@ -13,18 +13,19 @@ public class Foods
 public class Sjoeke_AI : AI_System
 {
     [SerializeField]
-    private List<GameObject> Tables = new List<GameObject>();
-    private List<Foods> AvailableFoods = new List<Foods>();
+    private List<Foods> SortedAvailableFoods = new List<Foods>();
+    private List<GameObject> AvailableFoods = new List<GameObject>();
 
     private int score;
 
     protected override void Awake()
     {
-        //FindFoods();
+        FindFoods();
     }
 
     protected override void Update()
     {
+        //base.Update();
 
     }
 
@@ -38,45 +39,40 @@ public class Sjoeke_AI : AI_System
         base.OnCollisionEnter(other);
     }
 
-    IEnumerator startFindingFood()
-    {
-        yield return new WaitForSeconds(1f);
-        FindFoods();
-    }
-    
     private void FindFoods()
     {
-        for (int i = 0; i < Tables.Count; i++)
+        AvailableFoods.Add(GameObject.FindGameObjectWithTag("Apple"));
+        AvailableFoods.Add(GameObject.FindGameObjectWithTag("Tomato"));
+        AvailableFoods.Add(GameObject.FindGameObjectWithTag("Egg"));
+        AvailableFoods.Add(GameObject.FindGameObjectWithTag("Chicken"));
+
+        for (int i = 0;  i < AvailableFoods.Count; i++) 
         {
-            if (Tables[i].transform.childCount > 0)
+            switch (AvailableFoods[i].gameObject.tag)
             {
-                Debug.Log("Hello in the first if");/*
-                switch (Tables[i].transform.GetChild(0).tag)
-                {
-                    case "Apple":
-                        score = 1;
-                        break;
+                case "Apple":
+                    score = 1;
+                    break;
 
-                    case "Tomato":
-                        score = 2;
-                        break;
+                case "Tomato":
+                    score = 2;
+                    break;
 
-                    case "Egg":
-                        score = 3;
-                        break;
+                case "Egg":
+                    score = 3;
+                    break;
 
-                    case "Chicken":
-                        score = 4;
-                        break;                    
-                }
-                Debug.Log(Tables[i].transform.GetChild(0));
-                AvailableFoods.Add(new Foods() { FoodName = Tables[i].transform.GetChild(0).name, FoodScore = score, FoodTransform = Tables[i].transform.GetChild(0).transform, FoodPosition = Tables[i].transform.GetChild(0).transform.position});  */              
+                case "Chicken":
+                    score = 4;
+                    break;                    
             }
-        }/*
-        //Debug.Log(AvailableFoods);
-        AvailableFoods.Sort(SortByScore);
-        movePositionTransform = AvailableFoods[0].FoodTransform;
-        GoToPosition();*/
+            Debug.Log(score);
+            //SortedAvailableFoods.Add(new Foods() { FoodName = food.name, FoodScore = score, FoodTransform = food.transform, FoodPosition = food.transform.position});            
+        }
+
+        //SortedAvailableFoods.Sort(SortByScore);
+        //movePositionTransform = SortedAvailableFoods[0].FoodTransform;
+        //GoToPosition();
     }
 
     private int SortByScore (Foods food1, Foods food2)
