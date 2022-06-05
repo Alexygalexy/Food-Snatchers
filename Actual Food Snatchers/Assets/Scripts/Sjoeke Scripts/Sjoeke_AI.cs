0c500e68cd5229164fbf5a2af6efd8f2e0be2245
Sjoeke <s.m.de.vries@st.hanze.nl>
2022-06-05 12:01:13 +0200

    Ai is getting the food, but still a lot of bugs

--- Actual Food Snatchers/Assets/Scripts/Sjoeke Scripts/Sjoeke_AI.cs
+++ Actual Food Snatchers/Assets/Scripts/Sjoeke Scripts/Sjoeke_AI.cs
@@ -2,6 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Sjoeke_AI : AI_System
+{
+    protected Transform FoodTarget;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        //RandomFood1();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        FoodTarget = RandomFood();
+
+        movePositionTransform = FoodTarget;
+    }
+
+    public override void GoToPosition()
+    {
+        if (movePositionTransform)
+        {
+            navMeshAgent.destination = movePositionTransform.position;
+        }
+        else
+        {
+            navMeshAgent.destination = transform.position;
+        }
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+
+    protected override void OnCollisionEnter(Collision other)
+    {
+        //StartCoroutine(FreezeEnemy(other));
+    }
+
+    IEnumerator FreezeEnemy(GameObject enemy)
+    {
+        yield return new WaitForSeconds(2f);
+    }
+    
+    protected List<GameObject> AvailableFood()
+    {
+        List<GameObject> foods = new List<GameObject>();
+
+        foods.AddRange(GameObject.FindGameObjectsWithTag("Apple"));
+        foods.AddRange(GameObject.FindGameObjectsWithTag("Tomato"));
+        foods.AddRange(GameObject.FindGameObjectsWithTag("Egg"));
+
+        return foods;
+    }
+    
+    protected Transform RandomFood()
+    {
+        int randomFoodInt = Random.Range(0, AvailableFood().Count);
+
+        GameObject randomFood = AvailableFood()[randomFoodInt];
+
+        Transform randomFoodTransform = randomFood.transform;
+
+        Debug.Log("Food is " + randomFood);
+        
+        return randomFoodTransform;
+    }/*
+
+    private void RandomFood1()
+    {
+        int randomFoodInt = Random.Range(0, AvailableFood().Count);
+
+        GameObject randomFood = AvailableFood()[randomFoodInt];
+
+        Debug.Log("Food is " + randomFood);
+        Transform randomFoodTransform = randomFood.transform;
+
+
+        movePositionTransform = randomFoodTransform;
+        GoToPosition();
+    }*/
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
 public class Foods
 {
     public string FoodName;
@@ -41,9 +132,6 @@ public class Sjoeke_AI : AI_System
         foods.AddRange(GameObject.FindGameObjectsWithTag("Egg"));
         foods.AddRange(GameObject.FindGameObjectsWithTag("Chicken"));
 
-        foreach (GameObject food in foods)
-            Debug.Log(food);
-
         return foods;
     }
 
@@ -73,7 +161,7 @@ public class Sjoeke_AI : AI_System
     {
         foreach (GameObject food in AvailableFoods()) 
         {
-            Debug.Log("helleo");/*
+            Debug.Log("helleo");
             switch (food.gameObject.tag)
             {
                 case "Apple":
@@ -94,11 +182,11 @@ public class Sjoeke_AI : AI_System
             }
             Debug.Log(score);
 
-            SortedAvailableFoods.Add(new Foods() { FoodName = food.name, FoodScore = score, FoodTransform = food.transform, FoodPosition = food.transform.position});            */
+            SortedAvailableFoods.Add(new Foods() { FoodName = food.name, FoodScore = score, FoodTransform = food.transform, FoodPosition = food.transform.position});            
         }
-        /*
+        
         SortedAvailableFoods.Sort(SortByScore);
-        Transform SortedFoodTransform = SortedAvailableFoods[0].FoodTransform;*/
+        Transform SortedFoodTransform = SortedAvailableFoods[0].FoodTransform;
 
         Transform SortedFoodTransform = table.transform;
 
@@ -108,7 +196,7 @@ public class Sjoeke_AI : AI_System
     private int SortByScore (Foods food1, Foods food2)
     {
         return food1.FoodScore.CompareTo(food2.FoodScore);
-    }
+    }*/
 
     #region Main Methods
 
@@ -119,4 +207,4 @@ public class Sjoeke_AI : AI_System
     //    base.OnTriggerEnter(other);
     //}
     #endregion
-}
+
