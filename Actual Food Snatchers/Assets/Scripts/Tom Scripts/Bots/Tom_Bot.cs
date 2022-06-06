70640877d5596a9d55dfb5bc20e5f94791210b5e
TomKlein88 <67271512+TomKlein88@users.noreply.github.com>
2022-06-06 16:35:30 +0200

    update Tom_Bot

--- Actual Food Snatchers/Assets/Scripts/Tom Scripts/Bots/Tom_Bot.cs
+++ Actual Food Snatchers/Assets/Scripts/Tom Scripts/Bots/Tom_Bot.cs
@@ -25,19 +25,20 @@ public class Tom_Bot : AI_System
         /*if(Apple_Prefab != null)
         {
             movePositionTransform.position = new Vector3(-13, -4, -22);
-        }*/ 
+        }*/
     }
 
     //My AI will not take Apple (except if it's directly on the road, so I don't need to get Apples.
-    protected List<GameObject> Foods()
+    protected List<GameObject> Targets()
     {
-        List<GameObject> foods = new List<GameObject>();
+        List<GameObject> target = new List<GameObject>();
 
-        foods.AddRange(GameObject.FindGameObjectsWithTag("Tomato"));
-        foods.AddRange(GameObject.FindGameObjectsWithTag("Egg"));
-        foods.AddRange(GameObject.FindGameObjectsWithTag("Chicken"));
+        target.AddRange(GameObject.FindGameObjectsWithTag("Tomato"));
+        target.AddRange(GameObject.FindGameObjectsWithTag("Egg"));
+        target.AddRange(GameObject.FindGameObjectsWithTag("Chicken"));
+        target.AddRange(GameObject.FindGameObjectsWithTag("Player"));
 
-        return (foods);
+        return (target);
     }
 
     public override void GoToPosition()
@@ -73,49 +74,47 @@ public class Tom_Bot : AI_System
     //Sometimes it doesn't work, probably because it's my first AI, so sorry in advance, I did my best.
     protected Transform Go_high_food()
     {
-        GameObject best_food = null;
+        GameObject best_target = null;
 
         float dist = Mathf.Infinity;
-        float value_apple = 1;
+        /*float value_apple = 1;
         float value_tomato = 2;
         float value_egg = 3;
-        float value_chicken = 4;
+        float value_chicken = 4;*/
 
         Vector3 pos = transform.position;
 
-        foreach (GameObject food in Foods())
+        foreach (GameObject target in Targets())
         {
-            Vector3 diff = food.transform.position - pos;
+            Vector3 diff = target.transform.position - pos;
             float cur_dist = diff.sqrMagnitude;
-            //print(food.name);
-            if (cur_dist < dist && food.name == "Chicken_Prefab(Clone)")
+            ///print(target.name);
+            if (cur_dist < dist && target.name == "Chicken_Prefab(Clone)")
             {
                 ///print("chicken");
-                best_food = food;
+                best_target = target;
                 dist = cur_dist;
             }
-            else if (cur_dist < dist && food.name == "Egg_Prefab(Clone)")
+            else if (cur_dist < dist && target.name == "Egg_Prefab(Clone)")
             {
                 ///print("egg");
-                best_food = food;
+                best_target = target;
                 dist = cur_dist;
             }
-            else if (cur_dist < dist && food.name == "Tomato_Prefab(Clone)")
+            else if (cur_dist < dist && target.name == "Tomato_Prefab(Clone)")
             {
                 ///print("tomato");
-                best_food = food;
+                best_target = target;
                 dist = cur_dist;
             }
-            else
+            /*else if (cur_dist < dist && food.name == "Test_Bot")
             {
-                go_snatch();
-            }
+                ///print("tomato");
+                best_target = food;
+                dist = cur_dist;
+            }*/
         }
-        Transform best_foodTransorm = best_food.transform;
-
-        return (best_foodTransorm);
+        Transform best_targetTransorm = best_target.transform;
+        return (best_targetTransorm);
     }
-
-    protected
-
 }
